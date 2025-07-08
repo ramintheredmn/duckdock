950bfbf56d85ffe8449fbad990ea13f2cb0ae51c
ramintheredmn <redcrossprog@gmail.com>
2025-07-09 01:50:49 +0330

    minor modification

--- get_pdb.py
+++ get_pdb.py
@@ -4,22 +4,36 @@ import pypdb
 import pandas as pd
 import inquirer
 
-def get_pdb(uniprot_id, exp_method="X-RAY DIFFRACTION", max_res=3, min_ligand_w=100):
+@redo.retriable(attempts=10, sleeptime=2)
+def get_pdb(uniprot_id, exp_method="X-RAY DIFFRACTION", max_res=float(3), min_ligand_w=100, max_chain=None):
 
     query_by_uniprot = rcsb.FieldQuery("rcsb_polymer_entity_container_identifiers.reference_sequence_identifiers.database_accession", exact_match=uniprot_id)
     query_by_expmethpd = rcsb.FieldQuery("exptl.method", exact_match=exp_method)
     query_by_res = rcsb.FieldQuery("rcsb_entry_info.resolution_combined", less_or_equal=max_res)
     query_by_ligand_mw = rcsb.FieldQuery("chem_comp.formula_weight", molecular_definition=True, greater=min_ligand_w)
+    query_by_numberOfChains = rcsb.FieldQuery("rcsb_entry_info.deposited_polymer_entity_instance_count", less_or_equal=max_chain)
 
-    query = rcsb.CompositeQuery(
-        [
-            query_by_uniprot,
-            query_by_res,
-            query_by_expmethpd,
-            query_by_ligand_mw
-        ],
-        "and"
-    )
+    if max_chain:
+        query = rcsb.CompositeQuery(
+            [
+                query_by_uniprot,
+                query_by_res,
+                query_by_expmethpd,
+                query_by_ligand_mw,
+                query_by_numberOfChains
+            ],
+            "and"
+        )
+    else:
+        query = rcsb.CompositeQuery(
+            [
+                query_by_uniprot,
+                query_by_res,
+                query_by_expmethpd,
+                query_by_ligand_mw
+            ],
+            "and"
+        )
 
     pdb_ids = rcsb.search(query)
     print(len(pdb_ids))
@@ -71,13 +85,23 @@ def extract_pdb_info(pdb_metadata_list):
     
     return results
 
-
+max_res = input("Enter the max resolution you want: (empty for 3) ")
+max_chain = input("Enter the max number of chains: (empty for not limiting) ")
 uniprot_id = input("Enter the uniprot id of the protein of interest: ")
-pdbs = get_pdb(uniprot_id)
+
+if len(max_res)>=1:
+    pdbs = get_pdb(uniprot_id, max_res=float(max_res))
+elif len(max_res) >=1 and len(max_chain)>=1:
+    pdbs = get_pdb(uniprot_id, max_res=float(max_res), max_chain=int(max_chain))
+elif len(max_chain) >= 1:
+    pdbs = get_pdb(uniprot_id, max_chain=float(max_chain))
+else:
+    pdbs = get_pdb(uniprot_id)
+
 pdb_descs = [describe_one_pdb_id(pdb_id) for pdb_id in pdbs]
 res = extract_pdb_info(pdb_descs)
 df = pd.DataFrame.from_dict(res)
-df_m = df.query("ligand_names != 'None' and ligand_count >= 1")
+df_m = df.query("ligand_names != 'None' and ligand_count >= 1").reset_index(drop=True)
 
 choises = [f"{i[0]}) {i[1]['pdb_id']}, resulotion: {i[1]['resolution']}, Chain count: {i[1]['num_chains']}, Ligand: {i[1]['ligand_names']}" for i in df_m.iterrows()]
 
@@ -87,6 +111,7 @@ questions = [inquirer.List(
     choices=choises,
 )]
 
-answer = inquirer.prompt(questions)
+answer= inquirer.prompt(questions)
 
-print(answer)
+print(answer.values()[0].split(')')[0])
+
